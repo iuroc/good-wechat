3dcf464ba3d1bb9e255502d063a7a99993547769
oyps <ponconsoft@gmail.com>
2023-01-11 16:53:20 +0800

    测试

---
+++ e.bat
@@ -0,0 +1,4 @@
+git add .
+set /p message=ÇëÊäÈëÏûÏ¢£º
+git commit -m %message%
+git push
--- wechat.php
+++ wechat.php
@@ -9,4 +9,8 @@ require('./good_wechat.php');
 $wechat = new Good_wechat();
 $wechat->start();
 // $wechat->send_text('你刚刚发送的“' . $wechat->content . '”暂时没有答案哦');
-$wechat->send_text('系统升级中，敬请谅解' . "\n\n" . '系统已设置部分保留关键词，你可以试试这些关键词' . "\n\n" . '更多资讯可以关注：<a href="https://apee.top">鹏优创官方网站</a>');
+$wechat->send_text('系统升级中，敬请谅解
+
+系统已设置部分保留关键词，你可以试试这些关键词
+
+更多资讯可以关注：<a href="https://apee.top">鹏优创官方网站</a>');
