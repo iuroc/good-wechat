ff9cdef955043999a6e5f3ba214b989698ea8605
oyps <ponconsoft@gmail.com>
2023-01-12 17:18:55 +0800

    ..

--- wechat.php
+++ wechat.php
@@ -11,9 +11,7 @@ $wechat->start();
 $wechat->add_rule('/.*?music.*?/', function ($args) {
     return '你输入的内容包含了 music 哦';
 });
-// $wechat->send_text('你刚刚发送的“' . $wechat->content . '”暂时没有答案哦');
+// 如果没有匹配结果，将直接回复下面的内容
 $wechat->send_text('系统升级中，敬请谅解
-
 系统已设置部分保留关键词，你可以试试这些关键词
-
 更多资讯可以关注：<a href="https://apee.top">鹏优创官方网站</a>');
