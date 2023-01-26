e78444e845fb71cc10219d709886e9d9a3bf8c4b
oyps <ponconsoft@gmail.com>
2023-01-27 01:23:14 +0800

    准备开发 DEMO 中的将 Token 插入数据库操作

--- demo/login_app/callback.php
+++ demo/login_app/callback.php
@@ -9,6 +9,12 @@ class Callback
     public string $token = '';
     /** 用户 ID */
     public string $user_id = '';
+    /** 配置信息 */
+    public array $config;
+    /** MySQL 数据库配置信息 */
+    public array $mysql_config;
+    /** 数据库连接 */
+    public mysqli $conn;
     public function __construct()
     {
         $this->token = $_GET['token'] ?? '';
@@ -16,16 +22,33 @@ class Callback
             echo 'token 值不能为空';
             die();
         }
+        $this->load_config(['../../../config.json']);
         $this->init_db();
         $this->check_token();
+    }
+    /** 载入配置信息 */
+    public function load_config(array $config_path)
+    {
+        $path = $config_path[0] ?? 'config.json';
+        $this->config = json_decode(file_get_contents($path), true);
+        $this->mysql_config = $this->config['mysql'];
     }
     /** 初始化第三方 Token 表 */
     public function init_db()
     {
         $sql = "CREATE TABLE IF NOT EXISTS `test_user_token` (
-            `user_id` VARCHAR(50),
-            `token` VARCHAR()
+            `user_id` VARCHAR(50) NOT NULL COMMENT '用户ID',
+            `token` VARCHAR(255) NOT NULL COMMENT 'Token 值',
+            `expiry_date` INT COMMENT '过期时间',
+            PRIMARY KEY (`user_id`)
         )";
+        $this->conn = mysqli_connect(
+            $this->mysql_config['host'],
+            $this->mysql_config['username'],
+            $this->mysql_config['password'],
+            $this->mysql_config['database']
+        );
+        mysqli_query($this->conn, $sql);
     }
     /** 远程校验 Token */
     public function check_token()
@@ -47,6 +70,7 @@ class Callback
     /** 增加第三方 Token 记录 */
     public function add_token()
     {
+        // $sql = "INSERT INTO `test_user_token`"
     }
 }
 
